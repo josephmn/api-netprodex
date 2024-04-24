189b29153ace8302de80c5dbc0f1fdac84c4a330
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-04-24 10:01:37 -0500

    creacion de metodo de guardar customer

--- src/main/java/com/netprodex/web/controller/CustomerController.java
+++ src/main/java/com/netprodex/web/controller/CustomerController.java
@@ -9,10 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,6 +36,12 @@ public class CustomerController {
         } else {
             return new ResponseEntity<>(customer, HttpStatus.OK);
         }
+    }
+
+    @Operation(summary = "Registry one customer")
+    @PostMapping(value = "/save", produces = "application/json")
+    public ResponseEntity<CustomerEntity> saveCustomer(@RequestBody CustomerEntity customer) {
+        return new ResponseEntity<>(this.customerServiceImpl.saveCustomer(customer), HttpStatus.CREATED);
     }
 
 }
