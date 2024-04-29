705a2b1d6f8f0e74ac2641ce02531b91078f1fd1
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-04-28 21:31:52 -0500

    creando entidad Permission

--- src/main/java/com/netprodex/persistence/entity/PermissionEntity.java
+++ src/main/java/com/netprodex/persistence/entity/PermissionEntity.java
@@ -1,2 +1,22 @@
-package com.netprodex.persistence.entity;public class PermissionEntity {
+package com.netprodex.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "permissions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PermissionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, updatable = false)
+    private String name;
 }
