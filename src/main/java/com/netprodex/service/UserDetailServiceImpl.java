23c48b58892aba8d2bca1efa78de102978dba506
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-04-28 23:36:43 -0500

    creacion de UserDetailServiceImpl para cargar usuarios de BD

---
+++ src/main/java/com/netprodex/service/UserDetailServiceImpl.java
@@ -0,0 +1,49 @@
+package com.netprodex.service;
+
+import com.netprodex.persistence.entity.UserEntity;
+import com.netprodex.persistence.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userEntity = this.userRepository.findUserEntityByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User: " + username + ", not exist."));
+
+        List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
+
+        userEntity.getRoles()
+                .forEach(role -> authorityList.add(new SimpleGrantedAuthority("ROLE_".concat(role.getRoleEnum().name()))));
+
+        userEntity.getRoles().stream()
+                .flatMap(role -> role.getPermissionList().stream())
+                .forEach(permission -> authorityList.add(new SimpleGrantedAuthority(permission.getName())));
+
+        return new User(userEntity.getUsername(),
+                userEntity.getPassword(),
+                userEntity.getIsEnabled(),
+                userEntity.getAccountNoExpired(),
+                userEntity.getCredentialNoExpired(),
+                userEntity.getAccountNoLocked(),
+                authorityList);
+    }
+}
