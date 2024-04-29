5fcd70c38237cf625e3b0163864acf9267064619
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-04-28 23:35:52 -0500

    creacion de UserRepository

--- src/main/java/com/netprodex/persistence/repository/UserRepository.java
+++ src/main/java/com/netprodex/persistence/repository/UserRepository.java
@@ -1,7 +1,15 @@
 package com.netprodex.persistence.repository;
 
 import com.netprodex.persistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findUserEntityByUsername(String username);
+
+//    @Query("SELECT u FROM UserEntity u WHERE u.username = ?")
+//    Optional<UserEntity> findUser(String username);
 }
