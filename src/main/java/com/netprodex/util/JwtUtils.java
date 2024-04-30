df51fed06febf2e26708f3529880128843a8b685
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-04-30 10:21:17 -0500

    creacion de JwtUtils para JWT Token

--- src/main/java/com/netprodex/util/JwtUtils.java
+++ src/main/java/com/netprodex/util/JwtUtils.java
@@ -1,2 +1,73 @@
-package com.netprodex.util;public class JwtUtils {
+package com.netprodex.util;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtils {
+    @Value("${security.jwt.key.private}")
+    private String privateKey;
+
+    @Value("${security.jwt.user.generator}")
+    private String userGenerator;
+
+    public String createToken(Authentication authentication) {
+        Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
+
+        String username = authentication.getPrincipal().toString();
+
+        String authorities = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        return JWT.create()
+                .withIssuer(this.userGenerator) // usuario backend que genera el token
+                .withSubject(username) // usuario a quien se le genera el token
+                .withClaim("authorities", authorities) // PAYLOAD:DATA: permisos
+                .withIssuedAt(new Date()) // PAYLOAD:DATA: fecha de creacion del token
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1800000)) // PAYLOAD:DATA: fecha de expiracion del token
+                .withJWTId(UUID.randomUUID().toString()) // PAYLOAD:DATA identificador random al token
+                .withNotBefore(new Date(System.currentTimeMillis())) // PAYLOAD:DATA: indicamos a partir de que hora sera valido el token, para el ejemplo es al mismo momentos
+                .sign(algorithm); // VERIFY SIGNATURE: firma de encriptacion con la firma
+    }
+
+    public DecodedJWT validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.privateKey);
+
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(this.userGenerator)
+                    .build();
+
+            return verifier.verify(token);
+        } catch (JWTVerificationException exception) {
+            throw new JWTVerificationException("Token invalid, not Authorized");
+        }
+    }
+
+    public String extractUsername(DecodedJWT decodedJWT) {
+        return decodedJWT.getSubject();
+    }
+
+    public Claim getSpecificClaim(DecodedJWT decodedJWT, String claimName) {
+        return decodedJWT.getClaim(claimName);
+    }
+
+    public Map<String, Claim> returnAllClaims(DecodedJWT decodedJWT) {
+        return decodedJWT.getClaims();
+    }
 }
